1317913e4c8d0a0a8af1550b35ef003f499c4dbf
lightsource <m2302070@edu.misis.ru>
2025-02-15 11:00:09 +0300

    add model+processor in func

--- ml/preprocess_image.py
+++ ml/preprocess_image.py
@@ -48,7 +48,7 @@ def load_model(size):
     return model, processor
 
 # image - PIL или numpy array
-def run_example(task_prompt, image, text_input=None):
+def run_example(model, processor, task_prompt, image, text_input=None):
     if text_input is None:
         prompt = task_prompt
     else:
@@ -73,47 +73,47 @@ def run_example(task_prompt, image, text_input=None):
 
 # получить описание изображения
 def get_caption(image):
-    return run_example('<CAPTION>', image)
+    return run_example(model, processor, '<CAPTION>', image)
 
 # получить детальное описание изображения
 def get_detailed_caption(image):
-    return run_example('<DETAILED_CAPTION>', image)
+    return run_example(model, processor, '<DETAILED_CAPTION>', image)
 
 # получить более детальное описание изображения
 def get_more_detailed_caption(image):
-    return run_example('<MORE_DETAILED_CAPTION>', image)
+    return run_example(model, processor, '<MORE_DETAILED_CAPTION>', image)
 
 # получить наивную детекцию всех объектов на фото (без конкретизации какие ищем)
 def get_od(image):
-    return run_example('<OD>', image)
+    return run_example(model, processor, '<OD>', image)
 
 # получить наивную детекцию всех объектов на фото (без конкретизации какие ищем)
 def get_od(image):
-    return run_example('<OD>', image)
+    return run_example(model, processor, '<OD>', image)
 
 # получить детекцию всех объектов на фото с их небольшими описаниями
 def get_dense_reg_caption(image):
-    return run_example('<DENSE_REGION_CAPTION>', image)
+    return run_example(model, processor, '<DENSE_REGION_CAPTION>', image)
 
 # получить детекцию всех объектов по заданному промпту (главное!!!), пример: инвалиды -> получаем bboxы с инвалидами
 def get_open_vocab_detection(image, prompt):
-    results = run_example('<OPEN_VOCABULARY_DETECTION>', image, prompt)
+    results = run_example(model, processor, '<OPEN_VOCABULARY_DETECTION>', image, prompt)
     bbox_results = convert_to_od_format(results['<OPEN_VOCABULARY_DETECTION>'])
     return results, bbox_results
 
 # получить детекцию объектов по описанию, пример: человек выкидывает мусор в урну -> bboxы человека, урны, мусора отдельно
 def get_detecton_with_desc(image, prompt):
-    pre_results = run_example('<CAPTION>', image)
+    pre_results = run_example(model, processor, '<CAPTION>', image)
     text_input = pre_results[task_prompt]
-    results = run_example('<CAPTION_TO_PHRASE_GROUNDING>', text_input)
+    results = run_example(model, processor, '<CAPTION_TO_PHRASE_GROUNDING>', text_input)
     results['<CAPTION>'] = text_input
     return results
 
 # получить детекцию объектов по описанию, пример: человек выкидывает мусор в урну -> bboxы человека, урны, мусора отдельно (более детально)
 def get_detecton_with_detailed_desc(image, prompt):
-    pre_results = run_example('<DETAILED_CAPTION>', image)
+    pre_results = run_example(model, processor, '<DETAILED_CAPTION>', image)
     text_input = pre_results[task_prompt]
-    results = run_example('<CAPTION_TO_PHRASE_GROUNDING>', text_input)
+    results = run_example(model, processor, '<CAPTION_TO_PHRASE_GROUNDING>', text_input)
     results['<DETAILED_CAPTION>'] = text_input
     return results
 
