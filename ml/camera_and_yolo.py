6abea10e9880b7635a0e35ef6766a079dda0552d
rdinit <28570294+rdinit@users.noreply.github.com>
2025-02-16 12:41:10 +0300

    cameras

---
+++ ml/camera_and_yolo.py
@@ -0,0 +1,34 @@
+import cv2
+import time
+
+cam_ip = '0.0.0.0:5'
+
+chan = 1
+cap = cv2.VideoCapture(f"rtsp://webstream:webstream2000@{cam_ip}/cam/realmonitor?channel={chan}&subtype=0&1")
+
+import requests
+
+url = 'http://127.0.0.1:8000/process-video'
+
+def send_video(filename, prompt):
+    files = [('file', open(filename, 'rb'))]
+    data = {"prompt": prompt}
+    resp = requests.post(url=url, data=data, files=files) 
+    print(resp.text)
+
+send_video('video.mp4', 'man in wheelchair')
+
+while True:
+    cap = cv2.VideoCapture(f"rtsp://webstream:webstream2000@{cam_ip}/cam/realmonitor?channel={chan}&subtype=0&1")
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        #time.sleep(2)
+        cv2.imshow(f'channel {chan}', frame)
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+    chan += 1
+    cap.release()
+    cv2.destroyAllWindows()
+
